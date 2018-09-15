ddfa439a771dd980c347148e3d6fff68d432e42c
jlhiskey <jasonhi@crazyredhead.com>
2018-09-15 15:51:25 -0700

    reader.js is working but im stuck one how to get the test to work.

--- __test__/reader.test.js
+++ __test__/reader.test.js
@@ -3,12 +3,29 @@
 const fileReader = require('../lib/reader');
 
 const litanyPath = `${__dirname}/../data/litany.txt`;
+const sherlockPath = `${__dirname}/../data/sherlock.txt`;
+const prideAndPrejudicePath = `${__dirname}/../data/pride-and-pred.txt`;
+const reallyBig = `${__dirname}/../data/reallybig.txt`;
+
+module.exports = {
+  setTestFrameworkScriptFile: './jest.setup.js',
+};
+
+jest.setTimeout(30000);
+
+const fileArray = [reallyBig, litanyPath, sherlockPath, prideAndPrejudicePath];
 
 describe('#reader.js', () => {
-  test('The first 5 characters of a file should be read', (done) => {
-    return fileReader.arrayReturn(litanyPath, 5, (data) => {
-      expect(data).toEqual('I mus');
+  test('The finalArray should return characters from each .txt file in order', (done) => {
+    return fileReader.readFiles(fileArray, (data) => {
+      expect(data).toEqual(['Reall', 'I mus', 'Proje', 'The P']);
       done();
     });
   });
+  // test('The finalArray null if there is no data', (done) => {
+  //     return fileReader.readFiles(fileArray, (data) => {
+  //         expect(data).toEqual(['Reall', 'I mus', 'Proje', 'The P']);
+  //         done();
+  //     });
+  // });
 });
--- main.js
+++ main.js
@@ -1,11 +1,1 @@
 'use strict';
-
-const fileReader = require('./lib/reader');
-
-const litanyPath = `${__dirname}/data/litany.txt`;
-const sherlockPath = `${__dirname}/data/sherlock.txt`;
-const prideAndPrejudicePath = `${__dirname}/data/pride-and-pred.txt`;
-
-const fileArray = [litanyPath, sherlockPath, prideAndPrejudicePath];
-
-// Im completely lost...
